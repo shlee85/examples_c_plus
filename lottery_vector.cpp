6dc9e9fc833f7606b21246e41ae63a6da2e71fd5
shlee85 <never4000@naver.com>
2020-12-18 13:19:26 +0900

    create Lottery random number

--- lottery_vector.cpp
+++ lottery_vector.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <vector>
+#include <random>
 
 #define LOTTO_MAX 7
 
@@ -18,7 +19,8 @@ private:
 	int lotto_num;
 	int lotto_idx;
 	int* check_list;
-	vector<int> vec;
+	vector<int> user_vec;
+	vector<int> machine_vec;
 
 public:
 	Lottery() {
@@ -30,19 +32,81 @@ public:
 			check_list[i] = 0;
 		}
 
-		vec.reserve(LOTTO_MAX);
+		user_vec.reserve(LOTTO_MAX - 1);
+		machine_vec.reserve(LOTTO_MAX);
 	}
 
-	bool duplicate_check(int num, vector<int>&);
-	void printLottoNumber(vector<int>& vec);
-	void Lotto_Input(vector<int>&);
-	void UserInterFace_Init();	
+	bool duplicate_check(int num, vector<int>& user_vec);
+	void printLottoNumber(vector<int>& user_vec);
+	void Lotto_Input(vector<int>& user_vec);
+	void UserInterFace_Init();
+
+	//로또 번호 생성, 본번호 + 보너스 번호
+	void CreateLotteryNumber(/*vector<int>& machine*/);
+	int BonusLotteryNumber(vector<int>& machine);
 
 	~Lottery() {
 		if (check_list)
 			delete[] check_list;
 	}
 };
+
+int Lottery::BonusLotteryNumber(vector<int>& vec)
+{
+	int rand_number;
+	int chk_flg = 0;
+
+	random_device rand;		//시드 값을 얻기 위한 random_device 생성.
+	mt19937_64 gen(rand());	//random_device를 통해 난수 생성 엔진을 초기화.	(메르센 트위스터 알고리즘 사용)
+
+	uniform_int_distribution<int> dis(1, 45); //1 ~ 45까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의 
+
+	rand_number = dis(gen);
+
+	//새롭게 발생된 보너스번호를 생성된 1등 번호와 충첩되는지 체크 한다.
+	//1등번호와 충첩되면 다시 보너스 번호를 생성 한다.
+	for (int i = 0; i < vec.size(); i++) {
+		if (rand_number == vec[i])
+			BonusLotteryNumber(vec);	
+	}
+
+	return rand_number; // 생성된 보너스번호 리턴.
+}
+
+void Lottery::CreateLotteryNumber(/*vector<int>& vec*/)
+{
+	//랜덤으로 1 ~ 45의 중복되지 않는 숫자를 생성 한다.
+	//보너스 번호는 별도로 생성. 1 ~ 45에서 중복되지 않는 값으로 생성 한다. 
+
+	int bonus_number = 0;
+	int create_lottery_num = 0;
+	random_device rand;		//시드 값을 얻기 위한 random_device 생성.
+	mt19937_64 gen(rand());	//random_device를 통해 난수 생성 엔진을 초기화.	(메르센 트위스터 알고리즘 사용)
+	
+	uniform_int_distribution<int> dis(1, 45); //1 ~ 45까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의 
+	
+	for (int i = 0; i < LOTTO_MAX - 1; i++) {
+		create_lottery_num = dis(gen);
+		
+		//중복 검사.
+		for (int j = 0; j < machine_vec.size(); j++) {
+			if (create_lottery_num == machine_vec[j])
+				CreateLotteryNumber();
+		}
+		machine_vec.push_back(create_lottery_num);
+	}
+
+	bonus_number = BonusLotteryNumber(machine_vec);
+
+	machine_vec.push_back(bonus_number);
+
+	cout << "생성된 1등번호 + 보너스번호 : ";
+	for (int i = 0; i < machine_vec.size(); i++) {
+		cout << "[" << machine_vec[i] << "]";
+	}
+
+	cout << endl;
+}
 
 bool check_number(int num)
 {
@@ -106,9 +170,11 @@ void Lottery::UserInterFace_Init()
 	cout << "총 3개를 맞추면 3등 입니다. " << endl;
 	cout << endl;
 
-	Lotto_Input(vec);
+	Lotto_Input(user_vec);
 
-	printLottoNumber(vec);
+	printLottoNumber(user_vec);
+
+	CreateLotteryNumber();
 }
 
 void Lottery::printLottoNumber(vector<int>& vec)
